a83c1ca793170381cbcee719359b1fc5b7a3b135
SusanSun8 <61705975+SusanSun8@users.noreply.github.com>
2020-11-20 22:09:12 +0800

    Modification
    - add multiple visits to a single station optional variable
    - ensure that each station is only visited when it has demand 
    - when truck is full, only visit station needs drop off; when truck is empty, only visit station needs pick up

--- Divvy_ACO.py
+++ Divvy_ACO.py
@@ -15,6 +15,7 @@ import random as rn
 from numpy.random import choice as np_choice
 from copy import deepcopy
 
+
 class Ant_Colony(object):
 
     def __init__(self, 
@@ -23,13 +24,13 @@ class Ant_Colony(object):
                  n_best, 
                  n_iterations, 
                  decay, 
-                 demand, 
+                 demand,
                  capacity, 
                  time_constraint = 60,
                  start_station=0, 
                  alpha=0.5, 
                  beta=1,
-                 verbose = False):
+                 multiple_visits = False):
         """
         Args:
             travel_time (2D numpy.array): Square matrix of travel_time. Diagonal is assumed to be np.inf.
@@ -42,6 +43,7 @@ class Ant_Colony(object):
             n_iterations: number of iterations run before it concludes on the best route
             demand(1d numpy.array): number of bikes demand at the dock, negative is demand, positive is surplus(overload)
             capacity(int): truck capacity
+            multiple_visits(bool): does the algorithms allows for multiple visits to a single station
             ant_colony = Ant_Colony(time_matrix, 100, 20, 2000, 0.95, demand = demand, alpha=1, beta=2)          
         """
         if start_station > len(travel_time):
@@ -59,24 +61,25 @@ class Ant_Colony(object):
         self.time_constraint = time_constraint
         self.demand = demand
         self.capacity = capacity
+        self.multiple_visits = multiple_visits
         self.all_time_best_path = ("placeholder", np.inf, 0, 0, self.demand)
-        self.verbose = verbose
 
     def run(self):
         best_path = None
-        all_time_best_path = ("placeholder", np.inf, 0, 0, self.demand)
+        all_time_best_path = ("placeholder", np.inf, 0, 0, self.demand,0,0)
         for i in range(self.n_iterations):
             all_paths = self.gen_all_paths()
             self.spread_pheromone(all_paths, self.n_best, best_path=best_path)
-            best_path = max(all_paths, key = lambda x: x[2]/x[1])
-            if self.verbose:
-                print('---------------------iteration ',i+1,'------------------------')
-                print('best_path: ', best_path[0])
-                print('time traveled', best_path[1])
-                print('satisfied customer', best_path[2])
-                print('unsatisfied customer', sum(abs(self.demand)) - best_path[2])
-                print('final vehicle inventory', best_path[3])
-                print('demand left', best_path[4])
+            best_path = max(all_paths, key = lambda x: x[2])
+            #print('---------------------iteration ',i+1,'------------------------')
+            #print('best_path: ', best_path[0])
+            #print('time traveled', best_path[1])
+            #print('satisfied customer', best_path[2])
+            #print('unsatisfied customer', sum(abs(self.demand)) - best_path[2])
+            #print('final vehicle inventory', best_path[3])
+            #print('demand left', best_path[4])
+            #print('bike pick up/ drop off action', best_path[5])
+            #print('truck inventory list', best_path[6])
             if best_path[2] > all_time_best_path[2]:
                 all_time_best_path = best_path  
             elif best_path[2] == all_time_best_path[2]:
@@ -95,33 +98,66 @@ class Ant_Colony(object):
             print('unsatisfied customer', sum(abs(self.demand)) - self.all_time_best_path[2])
             print('final vehicle inventory', self.all_time_best_path[3])
             print('demand left', self.all_time_best_path[4])
+            print('bike pick up/ drop off action', self.all_time_best_path[5])
+            print('truck inventory list', self.all_time_best_path[6])
 
     def update_vehicle(self, vehicle, satisfy, move, demand):
+        num_bikes_moved = 0 
+        #print('demand is :',demand[move])
         if vehicle + demand[move] <0:
             satisfy += vehicle
+            num_bikes_moved = -vehicle
             demand[move] += vehicle
             vehicle =0
         elif vehicle + demand[move] <= self.capacity:
             satisfy += abs(demand[move])
+            num_bikes_moved = demand[move]
             vehicle = vehicle + demand[move]
             demand[move] =0
+         
         else: # vehicle + self.demand[move] > self.capacity
             satisfy += self.capacity - vehicle
+            num_bikes_moved = self.capacity - vehicle
             demand[move] = demand[move]- (self.capacity - vehicle)
             vehicle = self.capacity
-        return satisfy, vehicle, demand
+        #print('bikes is:', num_bikes_moved)
+        return satisfy, vehicle, demand, num_bikes_moved
 
-    def pick_move(self, pheromone, dist, visited):
-        pheromone = np.copy(pheromone)
-        pheromone[list(visited)] = 0
-        row = pheromone ** self.alpha * (( 1.0 / dist) ** self.beta)
-        norm_row = row / row.sum()
-        move = np_choice(self.all_inds, 1, p=norm_row)[0]
+    def pick_move(self, pheromone1, dist, visited, vehicle,demand):
+        pheromone = np.copy(pheromone1)
+        # don't visit the position that were visited before
+        if not self.multiple_visits:
+            pheromone[list(visited)] = 0
+        
+        demand_arry = np.array(demand)
+        no_demand_lst = demand_arry ==0
+        no_demand_idx = np.where(no_demand_lst)[0]
+        # don't visit the position with no demand
+        pheromone[no_demand_idx] = 0
+        
+        # Determine the next move by the truck capacity
+        if vehicle == 0:
+            no_pick_up_station_lst = demand_arry<0
+            no_pick_up_station_idx = np.where(no_pick_up_station_lst)[0]
+            pheromone[no_pick_up_station_idx] =0
+        if vehicle == self.capacity:
+            no_drop_off_station_lst = demand_arry>0
+            no_drop_off_station_idx = np.where(no_drop_off_station_lst)[0]
+            pheromone[no_drop_off_station_idx] =0
+        
+        # if no more demand could be satisfied, truck back to origin
+        if sum(pheromone) == 0:
+            move =-1
+            #print('wow')
+        else:
+            row = pheromone ** self.alpha * (( 1.0 / dist) ** self.beta)
+            norm_row = row / row.sum()
+            move = np_choice(self.all_inds, 1, p=norm_row)[0]
         return move
     
     def spread_pheromone(self, all_paths, n_best, best_path):
         sorted_paths = sorted(all_paths, key=lambda x: x[1])
-        for path, dist, satisfy,__,__ in sorted_paths[:n_best]:
+        for path, dist, satisfy,__,__,__,__ in sorted_paths[:n_best]:
             for move in path:
                 # Objective: satisfy the most customers
                 self.pheromone[move] += 1/ ((sum(abs(self.demand))+1-satisfy)+1)
@@ -135,13 +171,15 @@ class Ant_Colony(object):
     def gen_all_paths(self):
         all_paths = []
         for i in range(self.n_ants):
-            path, satisfy, vehicle, demand = self.gen_path(self.start_station) 
-            all_paths.append((path, self.gen_path_dist(path), satisfy, vehicle, demand))
+            path, satisfy, vehicle, demand, bikes, truck_inv = self.gen_path(self.start_station) 
+            all_paths.append((path, self.gen_path_dist(path), satisfy, vehicle, demand, bikes, truck_inv))
         return all_paths
 
     
     def gen_path(self, start):
         path = []
+        bikes_moved =[]
+        truck_inv =[]
         visited = set()
         visited.add(start)
         prev = start
@@ -152,18 +190,25 @@ class Ant_Colony(object):
         satisfy = 0
         
         # pick up bikes from initial station  
-        satisfy, vehicle, demand = self.update_vehicle(vehicle, satisfy, start, demand)
+        satisfy, vehicle, demand, bikes = self.update_vehicle(vehicle, satisfy, start, demand)
+        truck_inv.append(vehicle)
+        bikes_moved.append(bikes)
             
         for i in range(len(self.travel_time) - 1):
-            move = self.pick_move(self.pheromone[prev], self.travel_time[prev], visited)
+            move = self.pick_move(self.pheromone[prev], self.travel_time[prev], visited, vehicle,demand)
             # check if we can take this move 
-            if remaining_travel_time > self.travel_time[prev][move]+self.travel_time[move][start]:
+            if move ==-1:
+                break
+            elif remaining_travel_time > self.travel_time[prev][move]+self.travel_time[move][start]:
                 remaining_travel_time -= self.travel_time[prev][move]
                 # calculate how much demand satisfied by this move & how the vehicle would change
-                satisfy, vehicle, demand = self.update_vehicle(vehicle, satisfy, move, demand)
+                satisfy, vehicle, demand,bikes = self.update_vehicle(vehicle, satisfy, move, demand)
+                #print('demand lst: ', demand)
                 path.append((prev, move))
                 prev = move
                 visited.add(move)
+                bikes_moved.append(bikes)
+                truck_inv.append(vehicle)
             else: 
             # there is not enough time left to travel to the next station
             # break and go back to the starting point directly
@@ -171,12 +216,9 @@ class Ant_Colony(object):
         path.append((prev, start)) # going back to where we started    
         remaining_travel_time -= self.travel_time[prev][start]
         
-        satisfy, vehicle, demand = self.update_vehicle(vehicle, satisfy, start, demand)
-        return path, satisfy, vehicle, demand
+        satisfy, vehicle, demand,bikes = self.update_vehicle(vehicle, satisfy, start, demand)
+        bikes_moved.append(bikes)
+        truck_inv.append(vehicle)
+        return path, satisfy, vehicle, demand, bikes_moved, truck_inv
     
-    
-
-
-
-
 

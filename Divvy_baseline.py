64d179bd660152c95fe5ed0bafbb5e3c74990b09
SusanSun8 <61705975+SusanSun8@users.noreply.github.com>
2021-02-11 00:27:29 -0600

    Add files via upload

--- Divvy_baseline.py
+++ Divvy_baseline.py
@@ -54,12 +54,12 @@ class Baseline(object):
     def run(self):
         # station related variables
         demand = list(deepcopy(self.demand))
+        demand_multiple_visit = list(deepcopy(self.demand))
         time_constraint = deepcopy(self.time_constraint)
         curr_time = 0
         visited = set()
-        prev = self.start_station
         capacity = self.capacity
-        visited.add(prev)
+        visited.add(self.start_station)
 
         paths = [[] for _ in range(self.num_trucks)]
         truck_inv = [[] for _ in range(self.num_trucks)]
@@ -68,6 +68,7 @@ class Baseline(object):
         truck_selected = [i for i in range(self.num_trucks)]
         bikes_on_trucks = [0 for _ in range(self.num_trucks)]
         satisfy = [0 for _ in range(self.num_trucks)]
+        prev_moves = [self.start_station for _ in range(self.num_trucks)]
 
         while curr_time < time_constraint:
             if len(truck_time_travel) < 1:
@@ -77,55 +78,56 @@ class Baseline(object):
             if curr_time > time_constraint:
                 break
             truck = truck_selected.pop(0)
+            prev = prev_moves[truck]
 
             # calculate maximum fulfillable capacity
             cur_num_bikes = bikes_on_trucks[truck]
-            high_bar = max(demand)  # 正数
-            low_bar = min(demand)  # 负数
+            if self.multiple_visits:
+                high_bar = max(demand)  # 正数
+                low_bar = min(demand)  # 负数
+            else:
+                high_bar = max(demand_multiple_visit)  # 正数
+                low_bar = min(demand_multiple_visit)  # 负数
 
             # demand 正数为pick-up，负数为drop-off
             pick_up_max = min(high_bar, capacity - cur_num_bikes) if high_bar > 0 else 0
             drop_off_max = min(-low_bar, cur_num_bikes) if low_bar < 0 else 0
 
+
             if pick_up_max == 0 and drop_off_max == 0:
                 truck_time_travel.append(next_move_time)
                 truck_selected.append(truck)
-                print(pick_up_max)
-                print(drop_off_max)
                 break
             elif pick_up_max >= drop_off_max:
                 move = demand.index(high_bar)
                 next_move_time = curr_time + self.travel_time[prev][move]
-                if next_move_time > time_constraint:
-                    break
-
-                demand[move] -= pick_up_max
-
-                bikes_on_trucks[truck] += pick_up_max
-                satisfy[truck] += pick_up_max
-
-                paths[truck].append(move)
-                truck_inv[truck].append(bikes_on_trucks[truck])
-                satisfy_record[truck].append(pick_up_max)
-                print(
-                'truck', truck, 'pick up', pick_up_max, 'bikes at station', move, ';finish time: ', next_move_time)
+                if next_move_time < time_constraint:
+                    demand[move] -= pick_up_max
+                    if not self.multiple_visits:
+                        demand_multiple_visit[move] = 0
+                    bikes_on_trucks[truck] += pick_up_max
+                    satisfy[truck] += pick_up_max
+                    paths[truck].append(move)
+                    truck_inv[truck].append(bikes_on_trucks[truck])
+                    satisfy_record[truck].append(pick_up_max)
+                    print(
+                    'truck', truck, 'pick up', pick_up_max, 'bikes at station', move, ';finish time: ', next_move_time)
 
             else:  # pick_up_max < drop_off_max
                 move = demand.index(low_bar)
                 next_move_time = curr_time + self.travel_time[prev][move]
-                if next_move_time > time_constraint:
-                    break
-                demand[move] += drop_off_max
+                if next_move_time < time_constraint:
+                    demand[move] += drop_off_max
+                    if not self.multiple_visits:
+                        demand_multiple_visit[move]=0
+                    bikes_on_trucks[truck] -= drop_off_max
+                    satisfy[truck] += drop_off_max
+                    paths[truck].append(move)
+                    truck_inv[truck].append(bikes_on_trucks[truck])
+                    satisfy_record[truck].append(drop_off_max)
 
-                bikes_on_trucks[truck] -= drop_off_max
-                satisfy[truck] += drop_off_max
-
-                paths[truck].append(move)
-                truck_inv[truck].append(bikes_on_trucks[truck])
-                satisfy_record[truck].append(drop_off_max)
-
-                print(
-                'truck', truck, 'drop_off', drop_off_max, 'bikes at station', move, ';finish time: ', next_move_time)
+                    print(
+                    'truck', truck, 'drop_off', drop_off_max, 'bikes at station', move, ';finish time: ', next_move_time)
             if self.num_trucks == 1:
                 truck_time_travel.append(next_move_time)
                 truck_selected.append(truck)
@@ -135,9 +137,8 @@ class Baseline(object):
             else:
                 truck_time_travel.append(next_move_time)
                 truck_selected.append(truck)
-            prev = move
+            prev_moves[truck] = move
 
-        print(demand)
         self.demand_lst = demand
         self.paths = paths
         self.truck_inv = truck_inv
@@ -152,4 +153,3 @@ class Baseline(object):
         print('paths: ', self.paths)
         print('truck inv', self.truck_inv)
         print('satisfied customer', sum(self.satisfy))
-        print('end time for trucks', self.truck_time_travel)
